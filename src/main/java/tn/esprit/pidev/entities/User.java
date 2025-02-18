2422fda1f86a46f97bd5e1614d7f15d0205ff212
abderahmenabssi <abderahmen.abssi@esprit.tn>
2025-02-18 10:00:47 +0100

    First Crud

--- src/main/java/tn/esprit/pidev/entities/User.java
+++ src/main/java/tn/esprit/pidev/entities/User.java
@@ -3,8 +3,7 @@ package tn.esprit.pidev.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
-@AllArgsConstructor
-@NoArgsConstructor
+
 @ToString
 @Setter
 @Getter
@@ -19,4 +18,64 @@ public class User {
     private String password;
     @Enumerated(EnumType.STRING)
     private Role userRole;
+
+    public long getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(long id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(Role userRole) {
+        this.userRole = userRole;
+    }
+
+    public User() {
+    }
+
+    public User(long id_user, String firstName, String lastName, String email, String password, Role userRole) {
+        this.id_user = id_user;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.userRole = userRole;
+    }
 }
